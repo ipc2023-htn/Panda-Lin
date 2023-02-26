84d5d3292994ee82671bb1cfbe4600c6b2429670
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-02-26 20:46:19 +1100

    add the build script

---
+++ build.py
@@ -0,0 +1,92 @@
+import os
+import subprocess
+from subprocess import CalledProcessError
+    
+def setup():
+    cwd = os.getcwd()
+    parserDir = os.path.join(
+            cwd, "pandaPIparser")
+    cmdForMakingParser = [
+            "make", "-C", 
+            parserDir]
+    _ = subprocess.run(
+            cmdForMakingParser)
+    try:
+        _.check_returncode()
+    except CalledProcessError:
+        exit(-1)
+    grounderDir = os.path.join(
+            cwd,
+            "pandaPIgrounder")
+    cpddlDir = os.path.join(
+            grounderDir,
+            "cpddl")
+    cmdForCpddl = [
+            "make", 
+            "-C",
+            cpddlDir,
+            "boruvka",
+            "opts",
+            "bliss",
+            "lpsolve"]
+    _ = subprocess.run(cmdForCpddl)
+    try:
+        _.check_returncode()
+    except CalledProcessError:
+        exit(-1)
+    cmdForMakingCpddl = [
+            "make",
+            "-C",
+            cpddlDir]
+    _ = subprocess.run(
+            cmdForMakingCpddl)
+    try:
+        _.check_returncode()
+    except CalledProcessError:
+        exit(-1)
+    cmdForMakingGrounder = [
+            "make",
+            "-C",
+            grounderDir]
+    _ = subprocess.run(
+            cmdForMakingGrounder)
+    try:
+        _.check_returncode()
+    except CalledProcessError:
+        exit(-1)
+    engineDir = os.path.join(
+            cwd,
+            "pandaPIengine")
+    engineBuildDir = os.path.join(
+            engineDir,
+            "build")
+    if not os.path.exists(engineBuildDir):
+        os.mkdir(engineBuildDir)
+    cmakeListsFile = os.path.join(
+            cwd,
+            "pandaPIengine",
+            "src")
+    cmdCMakeForEngine = [
+            "cmake", 
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-S",
+            cmakeListsFile,
+            "-D",
+            engineBuildDir]
+    _ = subprocess.run(cmdCMakeForEngine)
+    try:
+        _.check_returncode()
+    except CalledProcessError:
+        exit(-1)
+    cmdForMakingEngine = [
+            "make",
+            "-C",
+            engineBuildDir]
+    _ = subprocess.run(cmdForMakingEngine)
+    try:
+        _.check_returncode()
+    except CalledProcessError:
+        exit(-1)
+
+if __name__ == "__main__":
+    setup()
